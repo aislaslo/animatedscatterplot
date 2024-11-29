ca26a661e800a2aa3fcf92734be7e8d79b83ae20
aislaslo <islas.alex@gmail.com>
2024-11-28 18:25:21 -0600

    dawn commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import plotly.express as px
+
+data = px.data.gapminder()
+
+fig = px.scatter(
+    data,
+    x="gdpPercap",
+    y="lifeExp",
+    animation_frame="year",
+    animation_group="country",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=True,
+    size_max=60,
+    range_x=[200, 60000],
+    range_y=[20, 90],
+    title="Animated Scatter Plot: Life Expectancy vs GDP Per Capita"
+)
+
+fig.show()
